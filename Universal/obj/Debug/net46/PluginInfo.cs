3791217816dee7a75893946dcdc6c73246c7ef38
Snrasha <snrasha@gmail.com>
2024-01-07 10:25:24 +0100

    forget gitignore, remove univ temp

--- Universal/obj/Debug/net46/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace Universal
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "_Universal";
-        public const string PLUGIN_NAME = "_Universal";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
