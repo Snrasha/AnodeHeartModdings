591bab596b48989802768716b3cc4bb89887c913
Snrasha <snrasha@gmail.com>
2024-11-04 20:21:34 +0100

    update impossible mod

--- Impossible/Patches/Patch_Battle.cs
+++ Impossible/Patches/Patch_Battle.cs
@@ -35,21 +35,51 @@ namespace Impossible.Patches
     {
 
 
-        [HarmonyPatch(typeof(BattleDirector), nameof(BattleDirector.SetTP))]
-        static class Patch_BattleDirector_SetTP
+        //[HarmonyPatch(typeof(BattleDirector), nameof(BattleDirector.SetTP))]
+        //static class Patch_BattleDirector_SetTP
+        //{
+        //    [HarmonyPrefix]
+        //    public static bool Prefix(BattleDirector __instance,int tp, bool playerTurn)
+        //    {
+        //        if (playerTurn && Plugin.ImpossibleSubMenuGUI.config.tp_limit>0)
+        //        {
+        //            __instance.BattleTimeBar.SetTP(Plugin.ImpossibleSubMenuGUI.config.tp_limit - 1, playerTurn);
+        //            return false;
+        //        }
+        //        return true;
+        //    }
+
+        //}
+
+        [HarmonyPatch(typeof(BattleMonster), nameof(BattleMonster.StartTP), MethodType.Getter)]
+        static class Patch_BattleMonster_StartTP
         {
-            [HarmonyPrefix]
-            public static bool Prefix(BattleDirector __instance,int tp, bool playerTurn)
+            [HarmonyPostfix]
+            public static void PostFix(BattleMonster __instance,ref int __result)
             {
-                if (playerTurn && Plugin.ImpossibleSubMenuGUI.config.tp_limit>0)
+                if (!__instance.isEnemy && Plugin.ImpossibleSubMenuGUI.config.tp_limit>0)
                 {
-                    __instance.BattleTimeBar.SetTP(Plugin.ImpossibleSubMenuGUI.config.tp_limit - 1, playerTurn);
-                    return false;
+                    __result = Plugin.ImpossibleSubMenuGUI.config.tp_limit - 1;
                 }
-                return true;
             }
 
         }
+        //[HarmonyPatch(typeof(BattleDirector), nameof(BattleDirector.AddTP),MethodType.Enumerator)]
+        //static class Patch_BattleDirector_AddTP
+        //{
+        //    [HarmonyPrefix]
+        //    public static IEnumerator Prefix(BattleDirector __instance, int tp, bool playerTurn)
+        //    {
+        //        if (playerTurn && Plugin.ImpossibleSubMenuGUI.config.tp_limit > 0)
+        //        {
+        //            yield return __instance.BattleTimeBar.AddTP(Plugin.ImpossibleSubMenuGUI.config.tp_limit - 1, playerTurn);
+
+        //        }
+        //        yield return null;
+        //    }
+
+        //}
+
     }
 
 
